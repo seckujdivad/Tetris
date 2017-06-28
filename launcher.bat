c0e95d8c756b124f1ac729b587eacd8c9612a6ed
David Juckes <david.a.juckes@tesco.net>
2017-06-28 16:44:07 +0100

    Launcher now restarts the code after it runs to completion

--- launcher.bat
+++ launcher.bat
@@ -1,9 +1,15 @@
 @echo off
 
+:top
+
+cls
 title Tetris Launcher
 echo #### #### #### #### Tetris Launcher #### #### #### ####
 
 py %~dp0Tetris.py
 
 echo #### #### #### #### Run ended #### #### #### ####
+
+echo Restarting...
 pause
+goto top
