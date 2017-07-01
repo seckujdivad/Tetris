c2fde0168d0dd43d472a072850e265928f5c544d
David Juckes <david.a.juckes@tesco.net>
2017-07-01 22:08:06 +0100

    Added sql interface for settings

--- subsystems/settings.py
+++ subsystems/settings.py
@@ -11,7 +11,7 @@ class settings:
         self.item.label.music_on = tk.Label(self.left, text='Play music', font=fonts.small)
         self.item.label.reset = tk.Label(self.left, text='Reset all', font=fonts.small)
         #
-        self.item.interact.music_on = tk.Checkbutton(self.right, command=self.toggle_music)
+        self.item.interact.music_on = tk.Checkbutton(self.right, variable=self.values['music'])
         self.item.interact.reset = tk.Button(self.right, text='Reset', font=fonts.small, relief=fonts.relief, overrelief=fonts.overrelief, command=self.reset)
         #
         self.item.label.music_on.pack(anchor='nw')
@@ -19,12 +19,16 @@ class settings:
         #
         self.item.interact.music_on.pack(anchor='nw', fill=tk.X, expand=True)
         self.item.interact.reset.pack(anchor='nw', fill=tk.X, expand=True)
+        
         #
         self.title.pack(fill=tk.X)
         self.left.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
         self.right.pack(fill=tk.BOTH, side=tk.RIGHT, expand=True)
         self.functions.pack(fill=tk.BOTH)
         self.backbutton.pack(side=tk.BOTTOM, fill=tk.X)
+        #get all
+        for var in self.values:
+            self.values[var].set(self.get_data(var))
     def show_settings(self):
         start_menu.frame.pack_forget()
         self.frame.pack()
@@ -38,6 +42,13 @@ class settings:
             pass
     def reset(self):
         shutil.copyfile(paths.assets + 'persistent_default.db', 'persistent.db')
-    def toggle_music(self):
-        print(self.item.interact.music_on.get())
+    def get_data(self, name):
+        cursor = self.data.execute('SELECT name, data FROM settings')
+        all = cursor.fetchall()
+        for id, val in all:
+            if id == name:
+                return val
+    def write_data(self):
+        pass
+    values = {'music':tk.StringVar()}
 settings = settings()
